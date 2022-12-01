f7449a65f026c1bd69493a0983d1bcd6d98e9d7c
user123 <user123@Pavan>
2022-12-01 12:13:46 +0530

    hi

---
+++ ArrayGit/src/com/project/Array/Arraydemo.java
@@ -0,0 +1,20 @@
+package com.project.Array;
+
+public class Arraydemo {
+	
+	public static void main(String[] args) {
+		
+		int[] container=new int[5];
+		container[0]=32;
+		container[1]=22;
+		container[2]=58;
+		container[3]=43;
+		container[4]=66;
+		System.out.println(container[0] );
+		System.out.println(container[1] );
+		System.out.println(container[2] );
+		System.out.println(container[3] );
+		System.out.println(container[4] );
+	}
+
+}
